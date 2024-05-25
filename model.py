b6c8bf94e8461336b257adf929e2cfa866df6eeb
Isuru Yahampath <isuruyahampath1@gmail.com>
2024-05-25 14:51:44 +0530

    removed model

--- model.py
+++ model.py
@@ -1,10 +1,10 @@
 import numpy as np
 from tensorflow.keras.models import load_model
+import preprocess_image
 
 # Load your pre-trained CNN model
-model = load_model('path_to_your_model.h5')
+model = load_model('model/CXR_best_model.h5')
 
 def predict(image):
-    image = np.expand_dims(image, axis=0)  # Expand dims to fit model input shape
     prediction = model.predict(image)
     return prediction.tolist()  # Convert to list for JSON serialization
